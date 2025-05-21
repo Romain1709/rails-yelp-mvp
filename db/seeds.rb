33f664bf41f14e0393790845efc2aed910a72ed7
Riboullet Romain <riboullet.romain@outlook.fr>
2025-05-21 14:48:29 +0200

    exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Creating restaurants..."
+Restaurant.destroy_all
+
+restaurants = [
+  { name: "Epicure", address: "75008 Paris", phone_number: "01 45 61 12 12", category: "french" },
+  { name: "Sushi Shop", address: "75001 Paris", phone_number: "01 23 45 67 89", category: "japanese" },
+  { name: "Pasta e Basta", address: "75010 Paris", phone_number: "01 98 76 54 32", category: "italian" },
+  { name: "Dragon d'Or", address: "75013 Paris", phone_number: "01 55 44 33 22", category: "chinese" },
+  { name: "Chez Léon", address: "75002 Paris", phone_number: "01 22 33 44 55", category: "belgian" }
+]
+
+restaurants.each { |attrs| Restaurant.create!(attrs) }
+
+puts "Done!"
